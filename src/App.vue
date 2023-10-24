f59842587c2523eef1f9443edad846ccd3a30da3
Thassia <thassiadev@gmail.com>
2023-10-25 01:20:11 +0200

    chore: basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
-<script setup></script>
-
 <template>
-<div></div>
+  <HeaderComponent />
+  <DateComponent />
+  <TaskCreateComponent />
+
+  <div class="taskList">
+    <TaskItemComponent />
+  </div>
 </template>
+
+<script setup>
+import HeaderComponent from './components/HeaderComponent.vue'
+import DateComponent from './components/DateComponent.vue'
+import TaskCreateComponent from './components/TaskCreateComponent.vue'
+import TaskItemComponent from './components/TaskItemComponent.vue'
+</script>
 
 <style>
 body {
