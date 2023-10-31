4fb178a0bf6a9ecaa10b4ccf54a290b9e50f1dde
Thassia <thassiadev@gmail.com>
2023-10-31 22:56:25 +0100

    feat: task create component - style

--- src/components/TaskCreateComponent.vue
+++ src/components/TaskCreateComponent.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="taskCreateWrapper">
-    <label>
-      <font-awesome-icon :icon="['fas', 'pencil']" />
-      <input type="text" maxlength="28" v-model="taskName" />
-    </label>
+  <div class="task-create-wrapper">
+    <div class="task-create-input-wrapper">
+      <font-awesome-icon class="task-create-icon" :icon="['fas', 'pencil']" />
+      <input
+        class="task-create-input"
+        type="text"
+        maxlength="28"
+        placeholder="New Task"
+        v-model="taskName"
+      />
 
-    <button @click="addTask()" :disabled="disableButton()">
-      <font-awesome-icon :icon="['fas', 'plus']" />
-    </button>
+      <button class="task-create-button" @click="addTask()" :disabled="disableButton()">
+        <font-awesome-icon class="task-create-icon-plus" :icon="['fas', 'plus']" />
+      </button>
+    </div>
   </div>
 </template>
 
@@ -28,7 +34,52 @@ function disableButton() {
 </script>
 
 <style>
-.taskCreateWrapper {
-  border: 1px solid red;
+.task-create-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.task-create-input-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: #4a555814;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.task-create-input {
+  background: none;
+  outline: none;
+  border: none;
+
+  margin-left: 5px;
+  font-size: 1rem;
+  color: var(--gray);
+  padding: 5px;
+  font-weight: 100;
+}
+
+.task-create-input::placeholder {
+  opacity: 30%;
+}
+
+.task-create-icon {
+  color: var(--gray);
+  padding-left: 5px;
+}
+
+.task-create-button {
+  background: none;
+  border: none;
+}
+
+.task-create-icon-plus {
+  border-left: 1px solid var(--gray);
+  padding-left: 10px;
+  cursor: pointer;
+  font-size: 1rem;
 }
 </style>
